24c6db3c3be809c903e33e5ec3d56312565cad87
Olivia Appleton <liv@OliviaAppleton.com>
2022-11-02 17:32:53 -0400

    Fix OtherMarketValue._binary_value (#30)
    * Fix OtherMarketValue._binary_value
    
    * Changes for production server
    
    * remove stray breakpoint
    
    * Fix up `this`, update tests

--- src/rule/manifold/this.py
+++ src/rule/manifold/this.py
@@ -22,33 +22,36 @@ if TYPE_CHECKING:  # pragma: no cover
     from ...market import Market
 
 
+@define(slots=False)
 class ThisToOtherConverter(ManifoldMarketMixin):
     """A mixin class that converts market accesses to reuse `other` code."""
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        """Ensure that we override the market fetch methods."""
-        super().__init__(id_="N/A")
+    id_: str = "N/A"
 
     def api_market(self, client: Optional[ManifoldClient] = None, market: Optional[Market] = None) -> APIMarket:
         """Return an APIMarket object associated with this rule's market."""
         assert market is not None
+        market.refresh()
         return market.market
 
 
-class ThisMarketClosed(ThisToOtherConverter, OtherMarketClosed):
+@define(slots=False)
+class ThisMarketClosed(OtherMarketClosed, ThisToOtherConverter):
     """A rule that checks whether its associated market is closed."""
 
     def _explain_abstract(self, indent: int = 0, **kwargs: Any) -> str:
         return "If this market reaches its close date\n"
 
 
-class CurrentValueRule(ThisToOtherConverter, OtherMarketValue[T]):
+@define(slots=False)
+class CurrentValueRule(OtherMarketValue[T], ThisToOtherConverter):
     """Resolve to the current market-consensus value."""
 
     def _explain_abstract(self, indent: int = 0, **kwargs: Any) -> str:
         return "Resolves to the current market value\n"
 
 
+@define(slots=False)
 class UniqueTradersRule(ThisToOtherConverter, OtherMarketUniqueTraders):
     """Resolve to the current number of unique traders."""
 
