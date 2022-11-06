e870a8c20a8bae067df8bb6d86e2aa9780b0ab1b
Olivia Appleton <Liv@OliviaAppleton.com>
2022-11-05 22:42:36 -0400

    formatting

--- daemon.sh
+++ daemon.sh
@@ -12,14 +12,14 @@ do
 		while [ $min -ge 0 ]; do
 			while [ $sec -gt 0 ]; do
 				echo -ne "$(printf "%02d:%02d:%02d" $hour $min $sec)\033[0K\r"
-				let "sec=sec-1"
+				sec=sec-1
 				sleep 1
 			done
 			sec=59
-			let "min=min-1"
+			min=min-1
 		done
 		min=59
-		let "hour=hour-1"
+		hour=hour-1
 	done
 	trap INT
 done
